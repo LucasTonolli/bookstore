4af0f20d186930582836a377f576e8452276aeac
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-07 10:27:32 -0300

    feat: add update book action

---
+++ app/Actions/Books/UpdateBookAction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions\Books;
+
+use App\Models\Book;
+use Illuminate\Support\Facades\DB;
+
+class UpdateBookAction
+{
+    public function handle(Book $book, array $data): Book
+    {
+        return DB::transaction(function () use ($book, $data) {
+            /**
+             * Using $data directly in the update method will only update the fields that are present in the $data array.
+             */
+            $book->update($data);
+
+            if (isset($data['authors'])) {
+                $book->authors()->sync($data['authors']);
+            }
+
+            if (isset($data['genres'])) {
+                $book->genres()->sync($data['genres']);
+            }
+
+            return $book;
+        });
+    }
+}
