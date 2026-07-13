513da1c76984cd030d51547a963a3896c3536b99
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-13 16:33:28 -0300

    feat: implement User Registration

--- app/Http/Controllers/V1/Auth/RegisterController.php
+++ app/Http/Controllers/V1/Auth/RegisterController.php
@@ -3,19 +3,34 @@
 namespace App\Http\Controllers\V1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(RegisterRequest $request): JsonResponse
     {
+        $validated = $request->validated();
+        [$user, $token] = DB::transaction(function () use ($validated) {
+            $user = User::create($validated);
+
+            return [
+                $user,
+                $user->createToken('token')
+            ];
+        });
+
         return response()->json(status: Response::HTTP_CREATED, data: [
-            'pi'
+            'message' => 'User created successfully',
+            'token'  => $token->plainTextToken,
+            'data'  => UserResource::make($user)
         ]);
     }
 }
