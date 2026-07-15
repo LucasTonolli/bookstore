4ac303f2b94dc0df95f19497b85fee193098bc64
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-15 14:31:45 -0300

    feat: add middlewares in User controller

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class UserController extends Controller
+class UserController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [
+            new Middleware(middleware: 'auth:sanctum'),
+            new Middleware(middleware: ['abilities:user:create'], only: ['store']),
+            new Middleware(middleware: ['abilities:user:read'], only: ['index', 'show']),
+            new Middleware(middleware: ['abilities:user:update'], only: ['update']),
+            new Middleware(middleware: ['abilities:user:delete'], only: ['destroy']),
+        ];
+    }
     /**
      * Display a listing of the resource.
      */
