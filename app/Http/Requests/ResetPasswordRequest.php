5d4d75ef6ed3dac558b90dad4cc9a1bf0f07f7e8
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-16 16:54:13 -0300

    feat: create form request for password reset

---
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => ['required'],
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required', 'confirmed', 'min:5'],
+        ];
+    }
+}
