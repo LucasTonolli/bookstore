638d923389ea3f7270ec6eb221a1294ecca18daa
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-16 11:41:37 -0300

    feat: add interface in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use App\Enums\Roles;
 use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -15,7 +16,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
